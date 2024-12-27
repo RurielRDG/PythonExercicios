d257d685d3c4c154566bafb1d0daad15c27e6477
Ruriel <rurielrdg@gmail.com>
2024-12-27 17:22:20 -0300

    Ex003 criado

---
+++ ex003.py
@@ -0,0 +1,8 @@
+# Desafio 003
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1: int = int(input('Digite um número:'))
+n2 = int(input('Digite outro número:'))
+soma = n1 + n2
+#print('A soma entre', n1, n2, 'vale',soma)
+print('A soma entre {}, e {} é igual à {}'.format(n1, n2, soma))
