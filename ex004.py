f307a0c84281582c926aaf48043b982c92a64096
Ruriel <rurielrdg@gmail.com>
2024-12-28 17:38:09 -0300

    Mudanças variável

--- ex004.py
+++ ex004.py
@@ -1,15 +1,14 @@
 # Desafio 004
 # Faça um programa que leia algo pelo teclado e mostre o seu tipo primitivo e todos as informações possíveis sobre ele.
 
-alguma_coisa = input('Digite algo: ')
+algo = input('Digite algo: ')
 
-print('O tipo primitivo  é: {} '.format(type(alguma_coisa)))
-
-print('Só tem espaço? {}'.format(alguma_coisa.isspace()))
-print('É um número? {}'.format(alguma_coisa.isnumeric()))
-print('É alfabético? {}'.format(alguma_coisa.isalpha()))
-print('É alfanumérico? {}'.format(alguma_coisa.isalnum()))
-print('Está em maiúsculas? {}'.format(alguma_coisa.isupper()))
-print('Está em minúsculas? {}'.format(alguma_coisa.islower()))
-print('Está capitalizado? {}'.format(alguma_coisa.istitle()))
-print('A string está vazia ?',alguma_coisa.isprintable())
+print('O tipo primitivo  é: {} '.format(type(algo)))
+print('Só tem espaço? {}.'.format(algo.isspace()))
+print('É um número? {}.'.format(algo.isnumeric()))
+print('É alfabético? {}.'.format(algo.isalpha()))
+print('É alfanumérico? {}.'.format(algo.isalnum()))
+print('Está em maiúsculas? {}.'.format(algo.isupper()))
+print('Está em minúsculas? {}.'.format(algo.islower()))
+print('Está capitalizado? {}.'.format(algo.istitle()))
+print('A string está vazia ?',algo.isprintable())
